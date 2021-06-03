01ff40dee7471bb8db1c12ce9390060a8e810cc0
MohammadAnwarTabbaa <anwar.tabbaa94@gmail.com>
2021-06-03 19:23:39 +0300

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -67,4 +67,29 @@ JOIN employees on employees.Company= companies.Name
 WHERE employees.Role ="Graphic Designer"
 
 
+Count & Filter
+1)
+SELECT name
+FROM students
+WHERE Points=(SELECT max(Points) FROM students)
+
+2) 
+SELECT avg(Points)
+FROM students
+
+3)
+SELECT count(Points)
+FROM students
+WHERE Points=500
+
+4)
+SELECT name 
+FROM students 
+WHERE name like "%s%"
+
+5)
+SELECT *
+FROM students 
+ORDER BY Points DESC
+
 
