2235035b2e5f27755efcce58f1de88995a20b3d7
MohammadAnwarTabbaa <anwar.tabbaa94@gmail.com>
2021-06-03 18:08:38 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -15,4 +15,24 @@ WHERE students.name = "Basma";
 
 7- UPDATE students 
 SET Points = Points - 50 
-WHERE students.name = "Alex" ;  
+WHERE students.name = "Alex" ; 
+
+
+ 
+
+CREATE TABLE graduates (
+ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT ,
+Name varchar NOT NULL UNIQUE ,
+Age INT , 
+Gender varchar, 
+Points INTEGER,
+Graduation date
+);
+
+INSERT INTO graduates (Name,Age,Gender,Points) 
+SELECT Name , Age , Gender ,Points FROM students 
+WHERE students.Name = "Layal";
+
+UPDATE graduates
+SET Graduation ="2021-06-01"
+WHERE graduates.Name ="Layal"
